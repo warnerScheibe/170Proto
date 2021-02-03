af649b1f9d6b4d1f68a008911d634357f456fad8
tmoropou <tmoropou@ucsc.edu>
2021-02-03 03:56:57 -0800

    Deleted old backup of quarter 1 game

--- Proto01/Intermediate/Build/Win64/Proto01Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,1 +0,0 @@
-#include "D:/170/170Proto-005ef2f3d489e1438d4fb5c8042db3bd00ea3bd3/170Proto/Proto01/Intermediate/Build/Win64/Proto01Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- Proto01/Source/Proto01/Proto01.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Proto01.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Proto01, "Proto01" );
- 
